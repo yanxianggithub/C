726f8b87ecc4f025655ca03d629055d459dc7615
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2020-12-13 12:28:18 +0800

    set

--- C++/other/Time2.cpp
+++ C++/other/Time2.cpp
@@ -1,36 +1,70 @@
 #include<iostream>
+#include<Windows.h>
+#include<time.h>
 using namespace std;
 class Clock
 {
-private:
-    int hour,minute,second;
 public:
-    Clock(int h=0,int m=0,int s=0):hour(h),minute(m),second(s){}
-    Clock();
-    ~Clock(){}
+	int hour, minute, second;
+	Clock(int h, int m, int s) :hour(h), minute(m), second(s) {}
+	Clock();
+	~Clock() {}
 };
 
 Clock::Clock()
 {
-    time_t t;
-    struct tm tt;
-    t=time(NULL);
-    localtime_s(&tt,&t);
-    h=tt.tm_hour;
-    m=tt.tm_min;
-    s=tt.tm_sec;
+	time_t t;
+	struct tm tt;
+	t = time(NULL);
+	localtime_s(&tt, &t);
+	hour = tt.tm_hour;
+	minute = tt.tm_min;
+	second = tt.tm_sec;
 }
 
+void add_sec(Clock& C);
+void display_time(Clock& C);
 
-
-
-
-
-
-
-int mian()
+int main()
 {
+	//获取本地时间
+	Clock C;
+	//手动设置时间
+	//Clock C(2,3,4);
+	while (1)
+	{
+		display_time(C);
+		add_sec(C);
+		Sleep(1000);
+		system("cls");
+	}
+	return 0;
+}
 
+void add_sec(Clock& C)
+{
+	C.second++;
+	if (C.second >= 60)
+	{
+		C.minute++;
+		C.second = 0;
+		if (C.minute >= 60)
+		{
+			C.minute = 0;
+			C.hour++;
+			if (C.hour >= 12)
+			{
+				C.hour = 0;
+			}
 
+		}
 
+	}
+
+}
+
+void display_time(Clock& C)
+{
+	cout << C.hour << "时" << C.minute << "分"
+		<< C.second<< "秒" << endl;
 }
